592530908e41b3609ffaca438aaa3179b40a9c7b
egororachyov <egororachyov5@gmail.com>
2018-06-29 23:30:25 +0300

    Added Matrix2x2

--- Framework/Core/Maths/Matrix2x2.h
+++ Framework/Core/Maths/Matrix2x2.h
@@ -5,14 +5,119 @@
 #ifndef BERSERKENGINE_MATRIX2X2_H
 #define BERSERKENGINE_MATRIX2X2_H
 
+#include "../Essential/Types.h"
+#include "Vector2.h"
+
 namespace Berserk
 {
 
+    /**
+     * Matrix with size of 2x2 which stores its data in per string format and
+     * multiplies vectors via right side (M * v)
+     */
+    struct Matrix2x2
+    {
+    public:
+
+        /**
+         * Initialize matrix with 0
+         */
+        Matrix2x2();
+
+        /**
+         * Initialize matrix with values
+         *
+         * @param m11
+         * @param m12
+         * @param m21
+         * @param m22
+         */
+        Matrix2x2(float32 m11, float32 m12,
+                  float32 m21, float32 m22);
+
+        /**
+         * Get transposed matrix
+         *
+         * @return
+         */
+        Matrix2x2 GetTranspose();
+
+        /**
+         * Get matrix determinant
+         *
+         * @return
+         */
+        float32 GetDeterminant();
+
+        /**
+         * Return pointer to its internal array of values
+         *
+         * @return float* pointer to m
+         */
+        float* GetArray();
+
+        /**
+         * Assign via matrix M
+         *
+         * @param M
+         * @return
+         */
+        Matrix2x2 operator = (const Matrix2x2& M);
+
+        /**
+         * Per value addition (m[i] + M.m[i])
+         *
+         * @param M
+         * @return
+         */
+        Matrix2x2 operator + (const Matrix2x2& M);
+
+        /**
+         * Per value subtraction (m[i] - M.m[i])
+         *
+         * @param M
+         * @return
+         */
+        Matrix2x2 operator - (const Matrix2x2& M);
+
+        /**
+         * Multiplication of matrix (this * M)
+         *
+         * @param M
+         * @return
+         */
+        Matrix2x2 operator * (const Matrix2x2& M);
+
+        /**
+         * Multiplication via number a (m[i] * a)
+         *
+         * @param a
+         * @return
+         */
+        Matrix2x2 operator * (const float32 a);
+
+        /**
+         * Division by number a (m[i] / a)
+         *
+         * @param a
+         * @return
+         */
+        Matrix2x2 operator / (const float32 a);
+
+        /**
+         * get vector = this * v
+         *
+         * @param v
+         * @return
+         */
+        Vector2 operator * (const Vector2& v);
+
+    public:
+
+        float32 m[4];
+
+    };
+
 } // namespace Berserk
-
-class Matrix2x2 {
-
-};
-
 
 #endif //BERSERKENGINE_MATRIX2X2_H
--- Framework/Core/Sources/Matrix2x2.cpp
+++ Framework/Core/Sources/Matrix2x2.cpp
@@ -7,4 +7,81 @@
 namespace Berserk
 {
 
+    Matrix2x2::Matrix2x2()
+    {
+        m[0] = 0;
+        m[1] = 0;
+        m[2] = 0;
+        m[3] = 0;
+    }
+
+    Matrix2x2::Matrix2x2(float32 m11, float32 m12,
+                         float32 m21, float32 m22)
+    {
+        m[0] = m11;
+        m[1] = m12;
+        m[2] = m21;
+        m[3] = m22;
+    }
+
+    Matrix2x2 Matrix2x2::GetTranspose()
+    {
+        return Matrix2x2(m[0], m[2], m[1], m[3]);
+    }
+
+    float32 Matrix2x2::GetDeterminant()
+    {
+        return (m[0] * m[3] - m[1] * m[2]);
+    }
+
+    float32* Matrix2x2::GetArray()
+    {
+        return (float32*)m;
+    }
+
+    Matrix2x2 Matrix2x2::operator = (const Matrix2x2& M)
+    {
+        this->m[0] = M.m[0];
+        this->m[1] = M.m[1];
+        this->m[2] = M.m[2];
+        this->m[3] = M.m[3];
+
+        return *this;
+    }
+
+    Matrix2x2 Matrix2x2::operator + (const Matrix2x2& M)
+    {
+        return Matrix2x2(m[0] + M.m[0], m[1] + M.m[1],
+                         m[2] + M.m[2], m[3] + M.m[3]);
+    }
+
+    Matrix2x2 Matrix2x2::operator - (const Matrix2x2& M)
+    {
+        return Matrix2x2(m[0] - M.m[0], m[1] - M.m[1],
+                         m[2] - M.m[2], m[3] - M.m[3]);
+    }
+
+    Matrix2x2 Matrix2x2::operator * (const Matrix2x2& M)
+    {
+        return Matrix2x2(m[0] * M.m[0] + m[1] * M.m[2], m[0] * M.m[1] + m[1] * M.m[3],
+                         m[2] * M.m[0] + m[3] * M.m[2], m[2] * M.m[1] + m[3] * M.m[3]);
+    }
+
+    Matrix2x2 Matrix2x2::operator * (const float32 a)
+    {
+        return Matrix2x2(m[0] * a, m[1] * a,
+                         m[2] * a, m[3] * a);
+    }
+
+    Matrix2x2 Matrix2x2::operator / (const float32 a)
+    {
+        return Matrix2x2(m[0] / a, m[1] / a,
+                         m[2] / a, m[3] / a);
+    }
+
+    Vector2 Matrix2x2::operator * (const Vector2& v)
+    {
+        return Vector2(m[0] * v.x + m[1] * v.y, m[2] * v.x + m[3] * v.y);
+    }
+
 } // namespace Berserk
