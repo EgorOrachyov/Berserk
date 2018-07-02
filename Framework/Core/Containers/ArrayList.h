750aac41fb79c33477085ae72e6c00d2c123352e
egororachyov <egororachyov5@gmail.com>
2018-07-02 23:52:17 +0300

    Added ArrayList description

--- Framework/Core/Containers/ArrayList.h
+++ Framework/Core/Containers/ArrayList.h
@@ -14,93 +14,116 @@ namespace Berserk
 {
 
     /**
+     * Dynamic array for elements which allocates memory in the heap if its is not
+     * enough for next pushing of element. Should be used for element with default
+     * destructors for simple storing of that in this container.
      *
+     * @note If you want to store huge data with complex destructors look at
+     * Linked List container
      */
     template <typename Element> class ArrayList
     {
     public:
 
         /**
-         *
-         * @param initialSize
+         * Initialize list with desired start size
+         * @param initialSize (default value 16)
          */
         ArrayList(uint32 initialSize = 16);
 
         /**
-         *
+         * Calls empty
          */
         ~ArrayList();
 
         /**
+         * Add new element to the list
+         *
+         * @note Element will be stored in the list by value, not by reference
          *
          * @param element
          */
         void Add(const Element& element);
 
         /**
-         *
+         * Remove element with index
          * @param index
          */
         void Remove(uint32 index);
 
         /**
-         *
+         * Calls destructors for all the elements, frees buffer's data
          */
         void Empty();
 
         /**
+         * Reserve desired number of elements
+         *
+         * @warning Should be more than 16
+         * @warning If current number of elements more than desiredCount they will be destroyed
+         * and deleted from the list data
          *
          * @param desiredCount
          */
         void Reserve(uint32 desiredCount);
 
         /**
-         *
-         * @return
+         * Number of elements in the list
+         * @return Current count
          */
         uint32 GetSize() const;
 
         /**
-         *
-         * @return
+         * Max possible umber of elements in the list
+         * @return Max count
          */
         uint32 GetCapacity() const;
 
         /**
-         *
-         * @return
+         * Size of the element in the bytes
+         * @return Size in bytes
          */
         uint32 GetElementSize() const;
 
         /**
+         * Get element with index
+         *
+         * @warning Index should be in [0;size)
          *
          * @param index
-         * @return
+         * @return Elemnet
          */
         Element& Get(uint32 index) const;
 
         /**
+         * Get first element
          *
-         * @return
+         * @warning List should not be empty
+         *
+         * @return Element
          */
         Element& GetFirst() const;
 
         /**
+         * Get last element
          *
-         * @return
+         * @warning List should not be empty
+         *
+         * @return Element
          */
         Element& GetLast() const;
 
         /**
+         * Get pointer to internal elements buffer
          *
-         * @return
+         * @return Pointer to data
          */
         Element* GetBuffer() const;
 
     private:
 
         /**
-         *
+         * Will expand buffer by 2 if it is need
          */
         void ExpandBuffer();
 
@@ -157,9 +180,19 @@ namespace Berserk
         }
 
         mem_free(mBuffer);
-        mBuffer = 0;
+        mBuffer = NULL;
         mCapacity = 0;
         mCurrentSize = 0;
+    }
+
+    template <typename Element>
+    void ArrayList<Element>::Reserve(uint32 desiredCount)
+    {
+        if (desiredCount > 1)
+        {
+            mCapacity = desiredCount; // expand factor 2
+            mBuffer = (Element*)mem_realloc(mBuffer, mCapacity * sizeof(Element));
+        }
     }
 
     template <typename Element>
@@ -186,6 +219,21 @@ namespace Berserk
         ASSERT(index < mCurrentSize, "Index should be in [0;size)");
 
         return mBuffer[index];
+    }
+
+    template <typename Element>
+    Element& ArrayList<Element>::GetFirst() const
+    {
+        ASSERT(mCurrentSize > 0, "Array list is empty");
+
+        return mBuffer[0];
+    }
+
+    template <typename Element>
+    Element& ArrayList<Element>::GetLast() const
+    {
+        ASSERT(mCurrentSize > 0, "Array list is empty");
+        return mBuffer[mCurrentSize - 1];
     }
 
     template <typename Element>
