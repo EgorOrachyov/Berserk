05d19027a0d40aea2460d8d9bc763aacb085f8eb
egororachyov <egororachyov5@gmail.com>
2018-08-27 11:32:37 +0300

    Delete redundant stuff

--- Framework/Debug/NewFragmentLightningTesting.h
+++ Framework/Debug/NewFragmentLightningTesting.h
@@ -21,90 +21,6 @@
 
 using namespace Berserk;
 
-class RenderNodeActor : public Actor
-{
-public:
-
-    RenderNodeActor(const CStaticString& name, FLOAT32 lifeTime = 0) :
-            Actor(name, lifeTime)
-    {
-        Material* material = gRenderSystem->getMaterialManagerPtr()->createMaterial(CNAME("Mat1"));
-        RenderMesh* mesh = gRenderSystem->getRenderMeshManagerPtr()->createRenderMesh(CNAME("Mesh1"));
-
-        material->setType(MaterialType::MT_BASIC);
-        material->setAmbientComponent(Vector3f(0.01,0.01,0.01));
-        material->setDiffuseComponent(Vector3f(0.523,0.9967,0.252));
-        material->setSpecularComponent(Vector3f(0.1523,0.867,0.252));
-        material->setShininess(13);
-
-        //      v4 ------ v7
-        //     /|         /|
-        //    / |        / |
-        //   /  |       /  |
-        //  /   v5 ----/- v6
-        // v0 ------ v3   /
-        // |   /      |  /
-        // |  /       | /
-        // | /        |/
-        // v1 ------ v2
-
-        Vector3f n0 = Vector3f(1,0,0), n1 = Vector3f(-1,0,0),
-                 n2 = Vector3f(0,1,0), n3 = Vector3f(0,-1,0),
-                 n4 = Vector3f(0,0,1), n5 = Vector3f(0,0,-1);
-
-        Vector3f v0 = Vector3f(-1,1,1),  v1 = Vector3f(-1,-1,1),
-                 v2 = Vector3f(1,-1,1),  v3 = Vector3f(1,1,1),
-                 v4 = Vector3f(-1,1,-1), v5 = Vector3f(-1,-1,-1),
-                 v6 = Vector3f(1,-1,-1), v7 = Vector3f(1,1,-1);
-
-        const UINT32 data_count = 36;
-        VertexPN data[data_count] = {
-                {v0,n4},{v1,n4},{v2,n4},{v2,n4},{v3,n4},{v0,n4},
-                {v3,n0},{v2,n0},{v6,n0},{v6,n0},{v7,n0},{v3,n0},
-                {v7,n5},{v6,n5},{v5,n5},{v5,n5},{v4,n5},{v7,n5},
-                {v4,n1},{v5,n1},{v1,n1},{v1,n1},{v0,n1},{v4,n1},
-                {v4,n2},{v0,n2},{v3,n2},{v3,n2},{v7,n2},{v4,n2},
-                {v6,n3},{v2,n3},{v1,n3},{v1,n3},{v5,n3},{v6,n3}
-        };
-
-        const UINT32 index_count = 36;
-        UINT16  i[index_count] = {
-                0,1,2,3,4,5,
-                6,7,8,9,10,11,
-                12,13,14,15,16,17,
-                18,19,20,21,22,23,
-                24,25,26,27,28,29,
-                30,31,32,33,34,35
-        };
-
-        mesh->setType(MeshType::MT_PN);
-        mesh->addGeometryInfo(data, data_count);
-        mesh->addGeometryInfo(i, index_count);
-
-        renderNode = gRenderSystem->createRenderNode();
-        renderNode->setRenderNodeType(RenderNodeType::RNT_OBJECT);
-        renderNode->setRenderMesh(mesh);
-        renderNode->setMaterial(material);
-    }
-
-
-    virtual ~RenderNodeActor()
-    {
-        gRenderSystem->deleteRenderNode(renderNode);
-    }
-
-    void onUpdate(FLOAT64 delta) override
-    {
-        renderNode->setTransformation(rotate(Vector3f(0.8, 0.1, 0.63), 0.029) * renderNode->getTransformation());
-        gRenderSystem->queueRenderNode(renderNode);
-    }
-
-private:
-
-    RenderNode* renderNode;
-
-};
-
 class ActorCube : public Actor
 {
 public:
@@ -157,47 +73,6 @@ public:
 private:
 
     RenderNode* renderNode;
-
-};
-
-class ActorPlane : public Actor
-{
-public:
-
-    ActorPlane(const CStaticString& name, FLOAT32 lifeTime = 0) :
-            Actor(name, lifeTime)
-    {
-        RenderMesh* mesh = Plane::create(MeshType::MT_PN, 10, CNAME("MainPlane"));
-        Material* material = gRenderSystem->getMaterialManagerPtr()->createMaterial(CNAME("Mat2"));
-
-        material->setType(MaterialType::MT_BASIC);
-        material->setAmbientComponent(Vector3f(0.01,0.01,0.01));
-        material->setDiffuseComponent(Vector3f(0.5523,0.29967,0.9252));
-        material->setSpecularComponent(Vector3f(0.9,0.867,0.9));
-        material->setShininess(3);
-
-        renderNode = gRenderSystem->createRenderNode();
-        renderNode->setRenderMesh(mesh);
-        renderNode->setMaterial(material);
-        renderNode->setRenderNodeType(RenderNodeType::RNT_OBJECT);
-        renderNode->setTransformation(translate(Vector3f(0,-3,0)));
-    }
-
-
-    virtual ~ActorPlane()
-    {
-        gRenderSystem->deleteRenderNode(renderNode);
-    }
-
-    void onUpdate(FLOAT64 delta) override
-    {
-        gRenderSystem->queueRenderNode(renderNode);
-    }
-
-private:
-
-    RenderNode* renderNode;
-
 };
 
 class Layout : public Actor
