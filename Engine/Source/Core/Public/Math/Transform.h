6a5d2cf63650849f7dd12c9b4fc0b9ef0fc2663c
egororachyov <egororachyov5@gmail.com>
2019-02-07 17:06:43 +0300

    Add doc for Transform class

--- Engine/Source/Core/Public/Math/Transform.h
+++ Engine/Source/Core/Public/Math/Transform.h
@@ -11,20 +11,37 @@
 namespace Berserk
 {
 
+    /**
+     * Transform class representing from object to world transformation
+     * in the form os (scale, translation, rotation).
+     *
+     * Applies mentioned factors in the following order:
+     *
+     *      p = Matrix(translation) * Matrix(rotation) * Matrix(scale) * v
+     *
+     * Where: v - source point 4 component vector with w = 1
+     *        p - result point 4 component vector with w = 1
+     *
+     */
     class Transform
     {
     public:
 
+        /** Default transformation with 1.0 scale, id rotation and (0,0,0) translation */
         Transform();
 
         Transform(const Transform& t) = default;
 
-        Transform(const Vec3f& translation);
+        /** Default transformation with 1.0 scale, id rotation and chosen translation  */
+        explicit Transform(const Vec3f& translation);
 
+        /** From translation, rotation and scale == 1.0f (default) */
         Transform(const Vec3f& translation, const Quatf& rotation, float32 scale = 1.0f);
 
+        /** From translation, rotation and scale == 1.0f (default) */
         Transform(const Vec3f& translation, const Rotation& rotation, float32 scale = 1.0f);
 
+        /** From translation, rotation and scale == 1.0f (default) */
         Transform(const Vec3f& translation, const Mat4x4f& rotation, float32 scale = 1.0f);
 
         ~Transform() = default;
@@ -33,18 +50,24 @@ namespace Berserk
 
         Transform& operator= (const Transform& t) = default;
 
+        /** Add translation */
         void operator += (const Vec3f& t);
 
+        /** Sub translation */
         void operator -= (const Vec3f& t);
 
     public:
 
+        /** @return Reference to scale to modify it */
         float32& scale();
 
+        /** @return Reference to rotation to modify it */
         Rotation& rotation();
 
+        /** @return Reference to translation to modify it */
         Vec3f& translation();
 
+        /** @return Result transform in matrix form */
         Mat4x4f toMat4x4f();
 
     private:
