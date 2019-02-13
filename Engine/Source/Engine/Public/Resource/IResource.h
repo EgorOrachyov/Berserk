892fb4d159d3f6dfa213b04a9193f375bd27be02
egororachyov <egororachyov5@gmail.com>
2019-02-13 19:41:12 +0300

    Setup OpenGL lib and include, add IResource class

---
+++ Engine/Source/Engine/Public/Resource/IResource.h
@@ -0,0 +1,45 @@
+//
+// Created by Egor Orachyov on 13.02.2019.
+//
+
+#ifndef BERSERK_IRESOURCE_H
+#define BERSERK_IRESOURCE_H
+
+#include "Public/Misc/Types.h"
+
+namespace Berserk
+{
+
+    /**
+     * Interface for all CPU and GPU resources, which could be created
+     * in the time of engine execution. This class, its inheritors
+     * and associated resource managers is the part of the whole
+     * engine resource management system.
+     */
+    class IResource
+    {
+    public:
+
+        /** Initializes empty resource instance */
+        virtual void create() = 0;
+
+        /** Add reference to resource if (f.e. you copy that) */
+        virtual void addReference() = 0;
+
+        /** Try to delete resource if its reference count == 0 */
+        virtual void release() = 0;
+
+        /** @return References count to this resource */
+        virtual uint32 getReferenceCount() = 0;
+
+        /** @return Memory cost of this resource (on CPU side only) */
+        virtual uint32 getMemoryUsage() = 0;
+
+        /** @return String name of resource (its unique identifier) */
+        virtual const char* getName() = 0;
+
+    };
+
+} // namespace Berserk
+
+#endif //BERSERK_IRESOURCE_H
--- Engine/Source/Render/Public/Platform/ITexture.h
+++ Engine/Source/Render/Public/Platform/ITexture.h
@@ -5,11 +5,30 @@
 #ifndef BERSERK_ITEXTURE_H
 #define BERSERK_ITEXTURE_H
 
+#include "Public/Misc/Types.h"
+
 namespace Berserk
 {
 
     class ITexture
     {
+    public:
+
+        enum PixelFormat
+        {
+            FORMAT_R,
+            FORMAT_RG,
+            FORMAT_RGB,
+            FORMAT_RGBA,
+            FORMAT_DEPTH,
+            FORMAT_DEPTH_AND_STENCIL,
+        };
+
+    public:
+
+        virtual uint32 getWidth() = 0;
+
+        virtual uint32 getHeight() = 0;
 
     };
 
