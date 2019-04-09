f35a1851226800d02ebd8e7110aa15a06a278f4a
egororachyov <egororachyov5@gmail.com>
2019-04-09 10:15:22 +0300

    Add common spot and dir light sources blocks info

--- Engine/Shaders/Common/GLSLDefines.glsl
+++ Engine/Shaders/Common/GLSLDefines.glsl
@@ -3,3 +3,21 @@
 //
 
 #version 410 core
+
+// Define name: SPOT_LIGHTS_COUNT
+// Default value: 10
+// Description: Max number of active spot lights in one render pass
+
+#define SPOT_LIGHTS_COUNT
+
+// Define name: POINT_LIGHTS_COUNT
+// Default value: 10
+// Description: Max number of active point lights in one render pass
+
+#define POINT_LIGHTS_COUNT
+
+// Define name: DIRECTIONAL_LIGHTS_COUNT
+// Default value: 10
+// Description: Max number of active directional lights in one render pass
+
+#define DIRECTION_LIGHTS_COUNT
--- Engine/Shaders/Common/GLSLUniformBlocks.glsl
+++ Engine/Shaders/Common/GLSLUniformBlocks.glsl
@@ -4,29 +4,56 @@
 
 #version 410 core
 
-// Default cross-shader uniform block which contains
-// current mesh trasformations with needed matricies
+// Name:
+// Binding point[s]:
+// Description:
 
-layout (std140) uniform System
+// Name: System
+// Binding point[s]: 1
+// Description: Default cross-shader uniform block which contains 
+//              current mesh trasformations with needed matricies
+layout (std140) uniform System[Vert|Frag]
 {
-    mat4 Proj;
-    mat4 View;
-    mat4 Model;
-    mat4 ViewModel;
-    mat4 PVM;
+    mat4 Model;                                 // Current model transformation matrix
+    mat4 ViewModel;                             // View * Model    
+    mat4 PVM;                                   // Projection * View * Model
 }
 System;
 
-// Camera info unifirm block - scene info of active camera
-// (could be used for shadow mapping purposes)
-
-layout (std140) uniform Camera
+// Name: Camera
+// Binding point[s]: 2
+// Description: scene active camera info 
+//              (could be used for shadow mapping purposes)
+layout (std140) uniform Camera[Vert|Frag]
 {
-    vec4 Up;
-    vec4 Direction;
-    vec4 Position;
-    mat4 View;
-    mat4 Proj;
+    vec4 Up;                                    // World space Up vector
+    vec4 Direction;                             // World space direction vector
+    vec4 Position;                              // World space position
+    mat4 View;                                  // Game view matrix (camera view)
+    mat4 Projection;                            // Perspective or orthographic matrix for projection
 }
 Camera;
 
+// Name: SpotLightSource
+// Binding point[s]: 5
+// Description: Arrays with data about active spot light sources in the render pass
+layout (std140) uniform SpotLightSource[Vert|Frag]
+{
+    vec4 Position[];                            // World space position
+    vec4 Direction[];                           // World space direction
+    vec4 Intensity[];                           // Light source color
+    vec4 InnerOuterCutoff[];                    // Inner and outer cutodd for soft edges of the light
+    vec4 Distance_Epsilon_Flags_Sampler[];      // Packed: Distance of action, epsilon for soft shadows, flags and shadow map sampler id
+}
+SpotLightSpource;
+
+// Name: DirectionalLightSource
+// Binding point[s]: 7
+// Description: Arrays with data about active directional light sources in the render pass
+layout (std140) uniform DirectionalLightSource[Vert|Frag]
+{
+    vec4 Direction[];                           // World space direction
+    vec4 Intensity[];                           // Light source color
+    vec4 Flags_Sampler[];                       // Packed: flags and shadow map sampler id
+}
+DirectionalLightSource;
