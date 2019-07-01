2b706d45a4e43c99c30f934ba602f19aad986c76
egororachyov <egororachyov5@gmail.com>
2019-07-02 00:56:52 +0300

    Add console variable interface

---
+++ Engine/Source/CoreSystem/Public/Console/IConsoleObject.h
@@ -0,0 +1,72 @@
+//
+// Created by Egor Orachyov on 02.07.2019.
+//
+
+#ifndef BERSERK_ICONSOLEOBJECT_H
+#define BERSERK_ICONSOLEOBJECT_H
+
+#include <Misc/Bits.h>
+#include <Misc/Types.h>
+#include <Misc/UsageDescriptors.h>
+#include <Strings/String.h>
+
+namespace Berserk
+{
+
+    /**
+     * Modification/access types
+     */
+    enum CORE_API EConsoleObjectFlags
+    {
+        /** Set in the constructor as default */
+        Default     = SHIFT(0),
+
+        /** Cannot be modified */
+        ReadOnly    = SHIFT(1),
+
+        /** Could be called from any thread */
+        ThreadSafe  = SHIFT(2),
+
+        /** Could not been seen in release mode */
+        Cheat       = SHIFT(3)
+    };
+
+    /**
+     * Modification/call priority order
+     */
+    enum CORE_API EConsolePriority
+    {
+        /** Could be set values loaded from config files */
+        SetByConfig = 0,
+
+        /** Could be set via raw c++ call */
+        SetByCode = 1,
+
+        /** Could be set in console */
+        SetByConsole = 2,
+    };
+
+    /**
+     * Base class for console variable and console command objects
+     */
+    class CORE_API IConsoleObject
+    {
+    public:
+
+        /** @return Name of this console object */
+        virtual const String& getName() const = 0;
+
+        /** @return Multi-line help text for this object */
+        virtual const String& getHelp() const = 0;
+
+        /** @return Object access/behaviour flags */
+        virtual EConsoleObjectFlags getFlags() const = 0;
+
+        /** @return Modification/call priority for this object */
+        virtual EConsolePriority getPriority() const = 0;
+
+    };
+
+} // namespace Berserk
+
+#endif //BERSERK_ICONSOLEOBJECT_H
--- Engine/Source/CoreSystem/Public/Console/IConsoleVariable.h
+++ Engine/Source/CoreSystem/Public/Console/IConsoleVariable.h
@@ -5,14 +5,37 @@
 #ifndef BERSERK_ICONSOLEVARIABLE_H
 #define BERSERK_ICONSOLEVARIABLE_H
 
+#include <Console/IConsoleObject.h>
+
 namespace Berserk
 {
 
+    /**
+     * Console variable, that can be set/get via console manager.
+     */
+    class CORE_API IConsoleVariable
+    {
+    public:
+
+        /**
+         * Set the value of the variable from string
+         * @param value String representation of the value (raw value)
+         * @param priority Where/how access this value
+         * @return True if successfully set new value
+         */
+        virtual bool set(const char* value, EConsolePriority priority) = 0;
+
+        /** @return Variable data as int value */
+        virtual int32 getInt() const = 0;
+
+        /** @return Variable data as float value */
+        virtual float32 getFloat() const = 0;
+
+        /** @return Variable data as string value */
+        virtual const String& getString() const = 0;
+
+    };
+
 } // namespace Berserk
-
-class IConsoleVariable {
-
-};
-
 
 #endif //BERSERK_ICONSOLEVARIABLE_H
