3241039ada2c64b2d3522617dd4a540090ef2365
egororachyov <egororachyov5@gmail.com>
2019-08-02 00:47:18 +0300

    Add dirty setter for light base

--- Engine/Source/EngineSystem/Public/Components/LightBase.h
+++ Engine/Source/EngineSystem/Public/Components/LightBase.h
@@ -7,17 +7,25 @@
 
 #include <Misc/Types.h>
 #include <Misc/UsageDescriptors.h>
+#include <Misc/Bits.h>
 #include <Math/MathInclude.h>
 
 namespace Berserk
 {
 
     /** Engine build in light types */
-    enum ELightSourceType
+    enum ELightSourceType : uint8
     {
         LST_SpotLight,
         LST_PointLight,
         LST_DirectionalLight,
+    };
+
+    /** Flags to define which part of the light was changed */
+    enum ELightDirtyFlag : uint8
+    {
+        LDF_Transform = SHIFT(0),
+        LDF_Everything = SHIFT(1)
     };
 
     /** Base class for main and render thread light source types */
@@ -25,32 +33,86 @@ namespace Berserk
     {
     public:
 
+        /** Set this light cast shadows or not [cause main and render thread sync] */
+        void setCastShadows(bool castShadows)
+        {
+            mCastShadows = castShadows;
+            light_markDirty();
+        }
+
+        /** Set this light active (affects on the scene) [cause main and render thread sync] */
+        void setIsActive(bool active)
+        {
+            mIsActive = active;
+            light_markDirty();
+        }
+
+        /** Set light color [cause main and render thread sync] */
+        void setLightColor(const Vec3f& color)
+        {
+            mLightColor = color;
+            light_markDirty();
+        }
+
+        /** Set world position [cause main and render thread sync] */
+        void setWorldPosition(const Vec3f& position)
+        {
+            mWorldPosition = position;
+            light_markDirty(ELightDirtyFlag::LDF_Transform);
+        }
+
+        /** Set world rotation [cause main and render thread sync] */
+        void setWorldRotation(const Quatf& rotation)
+        {
+            mWorldRotation = rotation;
+            light_markDirty(ELightDirtyFlag::LDF_Transform);
+        }
+
+        /** Set max distance of affecting on objects[cause main and render thread sync] */
+        void setMaxLightDistance(float32 distance)
+        {
+            mLightMaxDistance = distance;
+            light_markDirty();
+        }
+
+        /** Set shadow map bias for proper sampling [cause main and render thread sync] */
+        void setShadowMapBias(float32 bias)
+        {
+            mShadowMapBias = bias;
+            light_markDirty();
+        }
+
         /** @return Type of this light source */
         ELightSourceType getLightSourceType() const { return mLightSourceType; }
 
         /** @return True if that light casts shadow */
-        bool castShadows() const                { return mCastShadows; }
+        bool castShadows() const { return mCastShadows; }
+
+        /** @return True if this light is active on the scene */
+        bool isActive() const { return mIsActive; }
 
         /** @return Color of this source */
-        const Vec3f& getLightColor() const      { return mLightColor; }
+        const Vec3f& getLightColor() const { return mLightColor; }
 
-        /** @return Relative position point */
-        const Vec3f& getLocalPosition() const   { return mLocalPosition; }
+        /** @return World space position */
+        const Vec3f& getWorldPosition() const { return mWorldPosition; }
 
-        /** @return Absolute position point */
-        const Vec3f& getWorldPosition() const   { return mWorldPosition; }
+        /** @return World space rotation */
+        const Quatf& getWorldRotation() const { return mWorldRotation; }
 
         /** @return Distance of action */
-        float32 getMaxLightDistance() const     { return mLightMaxDistance; }
+        float32 getMaxLightDistance() const { return mLightMaxDistance; }
 
         /** @return Depth map test bias */
-        float32 getShadowMapBias() const        { return mShadowMapBias; }
+        float32 getShadowMapBias() const { return mShadowMapBias; }
 
-        /** @return Percentage for camera distance where light source shadows could be visible  */
-        float32 getShadowActionFactor() const   { return mShadowActionFactor; }
+    private:
 
-        /** @return Shadow map buffer size */
-        uint32 getShadowMapSize() const         { return mShadowMapSize; }
+        /**
+         * Marks the light source data dirty and cause the following
+         * main and rendering thread synchronisation
+         */
+        virtual void light_markDirty(uint32 flags = ELightDirtyFlag::LDF_Everything) { };
 
     private:
 
@@ -66,26 +128,23 @@ namespace Berserk
         /** Color of the light source */
         Vec3f mLightColor = Vec3f(1.0f);
 
-        /** Relative position point */
-        Vec3f mLocalPosition = Vec3f(0.0f);
-
-        /** Absolute position point */
+        /** World space position */
         Vec3f mWorldPosition = Vec3f(0.0f);
+
+        /** World space rotation */
+        Quatf mWorldRotation = Quatf();
 
         /** Shows if that light casts shadow */
         bool mCastShadows;
+
+        /** Whether this light is active in the scene */
+        bool mIsActive;
 
         /** Distance of action */
         float32 mLightMaxDistance = 1.0f;
 
         /** Control depth map test mistake */
         float32 mShadowMapBias = DEFAULT_SHADOW_MAP_BIAS;
-
-        /** Percentage for camera distance where light source shadows could be visible */
-        float32 mShadowActionFactor = 1.0f;
-
-        /** Shadow map buffer size */
-        uint32 mShadowMapSize = DEFAULT_SHADOW_MAP_SIZE;
 
     };
 
